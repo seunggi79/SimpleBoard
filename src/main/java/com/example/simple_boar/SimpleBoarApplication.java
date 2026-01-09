b06271802227ba0ca6846b0d264352e49b4ea2d9
seunggi <hsg77689469.gamil.com>
2026-01-09 13:47:42 +0900

    init

---
+++ src/main/java/com/example/simple_boar/SimpleBoarApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple_boar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBoarApplication.class, args);
+	}
+
+}
