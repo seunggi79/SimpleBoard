45f0f1ad53b23e6734cdf1a3666e24ab087c41f4
seunggi <hsg77689469@gmail.com>
2026-01-24 00:51:26 +0900

    PostService

--- src/main/java/com/example/simple_boar/post/PostService.java
+++ src/main/java/com/example/simple_boar/post/PostService.java
@@ -2,6 +2,31 @@ package com.example.simple_boar.post;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostService {
+
+    PostRepository postRepository;
+
+    public PostService(PostRepository postRepository){
+        this.postRepository = postRepository;
+    }
+
+    public Post create(String title, String content){
+        Post post = new Post();
+        post.setTitle(title);
+        post.setContent(content);
+
+        Post saved = postRepository.save(post);
+        return saved;
+    }
+
+    public List<Post> findAll(){
+        return postRepository.findAll();
+    }
+
+    public Post findById(Long id){
+        return postRepository.findById(id);
+    }
 }
