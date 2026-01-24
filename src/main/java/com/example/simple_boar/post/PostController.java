7b3928f180488beecb1d2bb0a2770f6a88df4f5f
seunggi <hsg77689469@gmail.com>
2026-01-24 19:49:56 +0900

    createFromForm

--- src/main/java/com/example/simple_boar/post/PostController.java
+++ src/main/java/com/example/simple_boar/post/PostController.java
@@ -1,8 +1,9 @@
 package com.example.simple_boar.post;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/posts")
@@ -13,10 +14,40 @@ public class PostController {
         this.postService = postService;
     }
 
-    @RequestMapping("")
+    /*@RequestMapping("")
     public String main(){
         return "posts";
+    }*/
+
+    @GetMapping("/new")
+    public String newForm() {
+        return "posts/new"; // templates/posts/new.html
     }
 
+    @PostMapping("/new")
+    public String createFromForm(@RequestParam String title, @RequestParam String content){
+        postService.create(title, content);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/init")
+    @ResponseBody
+    public String init(){
+        postService.create("첫 번째 글", "첫 번째 내용");
+        postService.create("두 번째 글", "두 번째 내용");
+        return "ok";
+    }
+
+    @GetMapping
+    @ResponseBody
+    public List<Post> list(){
+        return postService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Post detail(@PathVariable Long id){
+        return postService.findById(id);
+    }
 
 }
