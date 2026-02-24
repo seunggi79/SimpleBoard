fb3d2fbfb26f548804512903aa22bdfb8123a3a1
seunggi <hsg77689469@gmail.com>
2026-02-24 17:54:00 +0900

    new init

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/posts": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-      },
-    },
-  },
-});
+})
