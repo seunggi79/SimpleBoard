c5d0b642ae58e5f9d149ee21064b0e58bcee2a49
seunggi <hsg77689469@gmail.com>
2026-02-24 21:04:12 +0900

    post entity 수정

--- backend/src/main/java/com/example/simple_board/post/domain/Post.java
+++ backend/src/main/java/com/example/simple_board/post/domain/Post.java
@@ -1,6 +1,8 @@
 package com.example.simple_board.post.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -9,9 +11,12 @@ import lombok.Data;
 public class Post {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
+
+    protected Post() {}
 
     public Post(String title, String content) {
         this.title = title;
